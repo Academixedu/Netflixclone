eaef0e180115e91cc7acc748316c09e274a4711e
gonchigars <gonchigars@gmail.com>
2024-10-16 11:21:21 +0530

    adding .env

--- src/redux/movieActions.js
+++ src/redux/movieActions.js
@@ -1,71 +1,78 @@
 // redux/movieActions.js
-import axios from 'axios';
+import axios from "axios";
+
+const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 
 export const fetchMovies = () => {
   return (dispatch) => {
-    dispatch({ type: 'FETCH_MOVIES_REQUEST' });
+    dispatch({ type: "FETCH_MOVIES_REQUEST" });
 
     axios
-      .get(`https://api.themoviedb.org/3/movie/popular?api_key=49a5508b99e54cbf67438655e1565e32`)
+      .get(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
       .then((response) => {
-        dispatch({ type: 'FETCH_MOVIES_SUCCESS', payload: response.data.results });
+        dispatch({
+          type: "FETCH_MOVIES_SUCCESS",
+          payload: response.data.results,
+        });
       })
       .catch((error) => {
-        dispatch({ type: 'FETCH_MOVIES_FAILURE', payload: error });
+        dispatch({ type: "FETCH_MOVIES_FAILURE", payload: error });
       });
   };
 };
 
 export const fetchMovieDetails = (movieId) => {
   return (dispatch) => {
-    dispatch({ type: 'FETCH_MOVIE_DETAILS_REQUEST' });
+    dispatch({ type: "FETCH_MOVIE_DETAILS_REQUEST" });
 
     axios
-      .get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=49a5508b99e54cbf67438655e1565e32`)
+      .get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}`)
       .then((response) => {
-        dispatch({ type: 'FETCH_MOVIE_DETAILS_SUCCESS', payload: response.data });
+        dispatch({
+          type: "FETCH_MOVIE_DETAILS_SUCCESS",
+          payload: response.data,
+        });
       })
       .catch((error) => {
-        dispatch({ type: 'FETCH_MOVIE_DETAILS_FAILURE', payload: error });
+        dispatch({ type: "FETCH_MOVIE_DETAILS_FAILURE", payload: error });
       });
   };
 };
 
-// redux/movieActions.js
-// ...
-
 export const searchMovies = (query) => {
-    return (dispatch) => {
-      dispatch({ type: 'SEARCH_MOVIES_REQUEST' });
-  
-      axios
-        .get(`https://api.themoviedb.org/3/search/movie?api_key=49a5508b99e54cbf67438655e1565e32&query=${query}`)
-        .then((response) => {
-          dispatch({ type: 'SEARCH_MOVIES_SUCCESS', payload: response.data.results });
-        })
-        .catch((error) => {
-          dispatch({ type: 'SEARCH_MOVIES_FAILURE', payload: error });
-        });
-    };
-  };
+  return (dispatch) => {
+    dispatch({ type: "SEARCH_MOVIES_REQUEST" });
 
-  // src/redux/movieActions.js
-// ...
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`
+      )
+      .then((response) => {
+        dispatch({
+          type: "SEARCH_MOVIES_SUCCESS",
+          payload: response.data.results,
+        });
+      })
+      .catch((error) => {
+        dispatch({ type: "SEARCH_MOVIES_FAILURE", payload: error });
+      });
+  };
+};
 
 export const fetchMoviesByCategory = (category) => {
-    return (dispatch) => {
-      dispatch({ type: 'FETCH_MOVIES_REQUEST' });
-  
-      axios
-        .get(`https://api.themoviedb.org/3/movie/${category}?api_key=49a5508b99e54cbf67438655e1565e32`)
-        .then((response) => {
-          dispatch({ type: 'FETCH_MOVIES_SUCCESS', payload: response.data.results });
-        })
-        .catch((error) => {
-          dispatch({ type: 'FETCH_MOVIES_FAILURE', payload: error });
-        });
-    };
-  };
-  
+  return (dispatch) => {
+    dispatch({ type: "FETCH_MOVIES_REQUEST" });
 
-  
+    axios
+      .get(`https://api.themoviedb.org/3/movie/${category}?api_key=${API_KEY}`)
+      .then((response) => {
+        dispatch({
+          type: "FETCH_MOVIES_SUCCESS",
+          payload: response.data.results,
+        });
+      })
+      .catch((error) => {
+        dispatch({ type: "FETCH_MOVIES_FAILURE", payload: error });
+      });
+  };
+};
